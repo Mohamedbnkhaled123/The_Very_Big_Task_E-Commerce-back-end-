d8b34af62328391e5537a70a5a4c288e83945c97
Velora Developer <developer@velora.com>
2026-08-08 20:29:07 +0300

    chore: add frontend vercel domain to CORS allowed origins

--- middlewares/cors.middleware.js
+++ middlewares/cors.middleware.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',').map(o => o.trim()).filter(Boolean)
-  : ['http://localhost:4200', 'http://localhost:8080', 'http://localhost:8081', 'http://localhost:8090'];
+  : ['http://localhost:4200', 'http://localhost:8080', 'http://localhost:8081', 'http://localhost:8090', 'https://customer-demo-e-commerce.vercel.app'];
 
 const corsOptions = {
   origin: (origin, callback) => {
